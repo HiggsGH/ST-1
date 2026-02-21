9404eef21160b2e1856063a642a37a1307c33fc7
Ilya Batushin <ilya.batushin@yandex.ru>
2026-02-21 19:27:10 +0300

    feat: add alg and tests

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,21 +4,47 @@
 
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
+  if (value <= 1)
+    return false;
+
+  for (uint64_t i = 2; i * i <= value; ++i) {
+    if (value % i == 0)
+      return false;
+  }
+
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+  if (n == 0)
+    return 0;
+
+  uint64_t count = 0;
+
+  for (uint64_t i = 2; ; i++) {
+    if (checkPrime(i) && ++count == n)
+      return i;
+  }
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+  uint64_t next_prime = value + 1;
+
+  while (true) {
+    if (checkPrime(next_prime))
+      return next_prime;
+    
+    next_prime++;
+  }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+  uint64_t sum = 0;
+
+  for (uint64_t i = 2; i < hbound; ++i) {
+    if (checkPrime(i))
+      sum += i;
+  }
+
+  return sum;
 }
--- test/tests.cpp
+++ test/tests.cpp
@@ -4,14 +4,91 @@
 #include <cstdint>
 #include "alg.h"
 
+// checkPrime тесты
 
-TEST(st1, sumPrime1) {
-  uint64_t res = sumPrime(2000000);
-  uint64_t expected = 142913828922;
-  EXPECT_EQ(expected, res);
+TEST(st1, checkPrime_1) {
+  EXPECT_FALSE(checkPrime(0));
 }
-TEST(st1, sumPrime2) {
-  uint64_t res = sumPrime(10);
-  uint64_t expected = 17;
-  EXPECT_EQ(expected, res);
+
+TEST(st1, checkPrime_2) {
+  EXPECT_FALSE(checkPrime(1));
+}
+
+TEST(st1, checkPrime_3) {
+  EXPECT_TRUE(checkPrime(2));
+}
+
+TEST(st1, checkPrime_4) {
+  EXPECT_TRUE(checkPrime(3));
+}
+
+TEST(st1, checkPrime_5) {
+  EXPECT_FALSE(checkPrime(4));
+}
+
+TEST(st1, checkPrime_6) {
+  EXPECT_TRUE(checkPrime(41));
+}
+
+// nPrime тесты
+
+TEST(st1, nPrime_1) {
+  EXPECT_EQ(nPrime(0), 0);
+}
+
+TEST(st1, nPrime_2) {
+  EXPECT_EQ(nPrime(1), 2);
+}
+
+TEST(st1, nPrime_3) {
+  EXPECT_EQ(nPrime(2), 3);
+}
+
+TEST(st1, nPrime_4) {
+  EXPECT_EQ(nPrime(40), 173);
+}
+
+// nextPrime тесты
+
+TEST(st1, nextPrime_1) {
+  EXPECT_EQ(nextPrime(0), 2);
+}
+
+TEST(st1, nextPrime_2) {
+  EXPECT_EQ(nextPrime(1), 2);
+}
+
+TEST(st1, nextPrime_3) {
+  EXPECT_EQ(nextPrime(2), 3);
+}
+
+TEST(st1, nextPrime_4) {
+  EXPECT_EQ(nextPrime(11), 13);
+}
+
+TEST(st1, nextPrime_5) {
+  EXPECT_EQ(nextPrime(1016), 1019);
+}
+
+// sumPrime тесты
+
+TEST(st1, sumPrime_1) {
+  EXPECT_EQ(sumPrime(0), 0);
+}
+
+TEST(st1, sumPrime_2) {
+  EXPECT_EQ(sumPrime(1), 0);
+}
+
+TEST(st1, sumPrime_3) {
+    EXPECT_EQ(sumPrime(2), 0);
+}
+TEST(st1, sumPrime_4) {
+  EXPECT_EQ(sumPrime(3), 2);
+}
+TEST(st1, sumPrime_5) {
+   EXPECT_EQ(sumPrime(40), 197);
+}
+TEST(st1, sumPrime_6) {
+  EXPECT_EQ(sumPrime(100), 1060);
 }
